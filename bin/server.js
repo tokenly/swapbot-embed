3e2aa9af4b63c7b6f2bf66fa62e44b0ae91939f7
Devon Weller <devon@tokenly.co>
2016-07-22 17:15:31 -0500

    Enable CORS

--- bin/server.js
+++ bin/server.js
@@ -6,9 +6,27 @@ let CACHE_LENGTH = process.env.CACHE_LENGTH || 3600;
 // 
 // Create a node-static server instance to serve the './public' folder 
 // 
-var file = new static.Server('./public', { cache: CACHE_LENGTH });
+var file = new static.Server('./public', {
+    cache: CACHE_LENGTH,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Method': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET',
+        'Access-Control-Allow-Headers': '*'
+    }
+});
  
 require('http').createServer(function (request, response) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    if ( request.method === 'OPTIONS' ) {
+        response.writeHead(200);
+        response.end();
+        return;
+    }
+
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
